90bdaafd209a4f8abe10970efa516fbf21686304
Unknown <saketh.kollu@gmail.com>
2019-09-01 19:15:32 -0700

    can set power

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import json
 
 pi = pigpio.pi()
 
+pins = {"red" : 17, "green" : 22, "blue" : 24}
+
 # Create a Server Socket and wait for a client to connect
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 6666))
@@ -14,7 +16,14 @@ while True:
     try:
         dataFromClient, address = server_socket.recvfrom(256)
         dataFromClient = dataFromClient.rstrip()
-        data = dict(dataFromClient)
+        
+        data = json.loads(dataFromClient)
+        
+        color = data["color"]
+        power = data["power"]
+
+        pi.set_PWM_dutycycle(pins[color], power)
+
     except Exception as e:
         print(e)
         print("Incorrect data recieved from client")
