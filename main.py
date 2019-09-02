193df8c204e5f98c38eb73c62f09e6697791e4ef
Unknown <saketh.kollu@gmail.com>
2019-09-01 17:22:23 -0700

    keyboard input

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from gpiozero import LED
 from time import sleep
+import keyboard
 
 red = LED(17)
 green = LED(22)
 blue = LED(24)
 
-red.on()
-green.on()
-blue.on()
+while True:
+    try:
+        if keyboard.is_pressed('r'):
+            red.on()
+            break
+        else:
+            red.off()
+    except:
+        break
