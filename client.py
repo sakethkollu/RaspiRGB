40e6f7871289e21e014f9b53ad4f278e64d14f1b
Unknown <saketh.kollu@gmail.com>
2019-09-01 18:48:16 -0700

    changed data type

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+IPADRESS = "192.168.1.14"
 while 1:
     try:
-        data = input("Enter Data :")
-        IPADRESS = "192.168.1.137"
-        # 6666 = Number Port
-        client_socket.sendto(data.encode(), (IPADRESS,6666))
+        color = input("Enter Color : ").lower()
+        power = input("Enter Power for " + color + " : ")
+        data = (color, power)
+        client_socket.sendto(data, (IPADRESS,6666))
         print ("Sending request")
     except Exception as ex:
         print(ex)
--- server.py
+++ server.py
@@ -1,44 +1,12 @@
 import socket
-from gpiozero import LED
-# GPIO Setting Up
-red = LED(17)
-green = LED(22)
-blue = LED(24)
+import pigpio
+
+pi = pigpio.pi()
 
 # Create a Server Socket and wait for a client to connect
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 6666))
 print ("UDPServer Waiting for client on port 6666")
-
-# Define moving functions
-
-def red_on():
-    red.on()
-
-def green_on():
-    green.on()
-
-def blue_on():
-    blue.on()
-
-def red_off():
-    red.off()
-
-def green_off():
-    green.off()
-
-def blue_off():
-    blue.off()
-
-
-
-options = {    "0" : red_on,
-               "1" : red_off,
-               "2" : green_on,
-               "3" : green_off,
-               "4" : blue_on,
-               "5" : blue_off,
-}
 
 # Recive data from client and decide which function to call
 while True:
@@ -46,7 +14,6 @@ while True:
         dataFromClient, address = server_socket.recvfrom(256)
         dataFromClient = dataFromClient.rstrip()
         print(dataFromClient)
-        options[dataFromClient]()
     except Exception as e:
         print(e)
         print("Incorrect data recieved from client")
